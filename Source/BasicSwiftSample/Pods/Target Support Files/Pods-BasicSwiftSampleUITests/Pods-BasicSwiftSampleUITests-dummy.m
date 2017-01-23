24b001cee6ac5c4f9736a3f91c73de53d45c9da7
RAHUL CK <rahulvatakara@gmail.com>
2017-01-23 18:38:42 +0530

    initial commit

---
+++ Source/BasicSwiftSample/Pods/Target Support Files/Pods-BasicSwiftSampleUITests/Pods-BasicSwiftSampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BasicSwiftSampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BasicSwiftSampleUITests
+@end
---
+++ Source/BasicSwiftSample/Pods/Target Support Files/Pods-BasicSwiftSampleUITests/Pods-BasicSwiftSampleUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BasicSwiftSampleUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BasicSwiftSampleUITestsVersionString[];
+
